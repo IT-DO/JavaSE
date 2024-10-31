f5e243672ec91f46507189ac6943fc76ee4b050e
Anton Kurischev <anton.kurishchev@yandex.ru>
2024-10-31 19:55:19 +0300

    formatted code

--- src/Java_Core/InputOutputTests/BufferedReaderAndWriter.java
+++ src/Java_Core/InputOutputTests/BufferedReaderAndWriter.java
@@ -4,11 +4,11 @@ import java.io.*;
 
 public class BufferedReaderAndWriter {
     public static void main(String[] args) {
-        try(BufferedReader breader = new BufferedReader(new FileReader("res/sourcefile.txt"));
-            BufferedWriter bwriter = new BufferedWriter(new FileWriter("res/testfile.txt"))){
-            while(breader.ready()) {
-                String line = breader.readLine()+"\n";
-                bwriter.write(line,0,line.length());
+        try (BufferedReader breader = new BufferedReader(new FileReader("res/sourcefile.txt"));
+             BufferedWriter bwriter = new BufferedWriter(new FileWriter("res/testfile.txt"))) {
+            while (breader.ready()) {
+                String line = breader.readLine() + "\n";
+                bwriter.write(line, 0, line.length());
                 System.out.println(line);
             }
         } catch (FileNotFoundException e) {
